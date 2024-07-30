7abd9be1c2dc34a4f7d7d9117e9137fc509ab1f3
Givr <lightpower@hotmail.fr>
2024-07-31 01:54:27 +0200

    hop

---
+++ src/components/skate-card.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <img alt="skate-board" class="h-50 w-50" src="@/assets/board/red-dead/IMG_8330.jpg"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
+import SkateCard from "@/components/skate-card.vue";
 </script>
 
 <template>
   <main class="w-screen h-screen bg-black">
+    <SkateCard/>
   </main>
 </template>
