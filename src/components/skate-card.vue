2a28a9527cd0b53a9e7c814673e4fc4a7d585bb2
Givr <lightpower@hotmail.fr>
2024-08-01 01:57:10 +0200

    fix img import

--- src/components/skate-card.vue
+++ src/components/skate-card.vue
@@ -8,11 +8,9 @@ defineProps({
 </script>
 
 <template>
-  <div class="m-4 ">
-    <div class="relative w-80 h-96 text-center flex flex-col justify-center items-center">
-      <img alt="skate-board" class="top-0 absolute z-10" src="@/assets/overlay.png"/>
-      <img :src="imgPath" alt="skate-board" class="top-0 absolute"/>
-    </div>
+  <div class="m-4 relative w-80 h-96 text-center flex shadow-xl flex-col justify-center items-center">
+    <img alt="skate-board" class="top-0 absolute z-10 rounded" src="@/assets/overlay.png"/>
+    <img :src="imgPath" alt="skate-board" class="top-0 rounded absolute"/>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
 import SkateCard from "@/components/skate-card.vue";
+import witcherMinia from "@/assets/board/witcher/minia.jpg";
+import rdrMinia from "@/assets/board/red-dead/minia.jpg";
 </script>
 
 <template>
   <main class="w-screen h-screen bg-neutral-800 flex flex-column justify-center items-center">
-    <SkateCard img-path="src/assets/board/red-dead/minia.jpg"/>
-    <SkateCard img-path="src/assets/board/witcher/minia.jpg"/>
+    <SkateCard :img-path="rdrMinia"/>
+    <SkateCard :img-path="witcherMinia"/>
   </main>
 </template>
