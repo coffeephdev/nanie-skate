54fc55ee1af98d602cec7b8e448b9cdd67d19779
Givr <lightpower@hotmail.fr>
2024-08-02 01:24:30 +0200

    loadings for images

--- src/views/SkateView.vue
+++ src/views/SkateView.vue
@@ -9,14 +9,24 @@ import {computed, ref} from "vue";
 
 const skateStore = useSkateStore()
 
-const loading = ref(false)
-
-const images = computed(() => {
-  loading.value = true
-  const result = skateStore.getImagesPath
-  loading.value = false
-  return result
+const imagesLoader = ref<boolean[]>([])
+const isLoading = computed(() => {
+  return imagesLoader.value.some(loading => loading == true)
 })
+
+function handleStartLoading(index: number) {
+  imagesLoader.value[index] = true;
+}
+
+function handleLoading(index: number) {
+  imagesLoader.value[index] = false;
+  console.log(`Image ${index + 1} loaded successfully`);
+}
+
+const handleError = (index: number) => {
+  imagesLoader.value[index] = false;
+  console.log(`Failed to load image ${index + 1}`);
+};
 </script>
 
 <template>
@@ -24,22 +34,24 @@ const images = computed(() => {
     <SkateCard v-for="info in skateStore.skateInfos" :choice="info.skateChoice" :img-path="info.miniaturePath"/>
 
     <n-modal v-model:show="skateStore.showGallery"
-             :loading="loading"
-             :title="`${skateStore.getLabel}`"
+             :title="`Planche Custom - ${skateStore.getLabel}`"
              bordered
              class="max-h-[80%] max-w-[80%] overflow-y-auto text-center"
              close-on-esc
              preset="card"
              @close="skateStore.resetGallery()"
     >
-      <n-spin :show="loading">
+      <n-spin :show="isLoading">
         <n-image-group>
           <n-image
-              v-for="imagePath in images"
-              :alt="skateStore.getLabel + imagePath"
+              v-for="(imagePath, index) in skateStore.getImagesPath"
+              :alt="`${skateStore.getLabel} img${index}`"
               :src="imagePath"
               class="w-[40%] max-h-80 m-4 object-cover hover:outline rounded hover:outline-1 hover:scale-105 transition-all"
               object-fit="cover"
+              @error="handleError(index)"
+              @load="handleLoading(index)"
+              @loadstart="handleStartLoading(index)"
           />
         </n-image-group>
       </n-spin>
