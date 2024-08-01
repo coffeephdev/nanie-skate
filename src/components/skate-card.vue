4b6550d22743221feab5761e60faaaaf4c6ecba4
p.houot <>
2024-08-01 19:40:20 +0200

    nice update imho

--- src/components/skate-card.vue
+++ src/components/skate-card.vue
@@ -1,19 +1,61 @@
 <script lang="ts" setup>
+import Tilt from 'vanilla-tilt-vue'
+
 defineProps({
   imgPath: {
     type: String,
     required: true
   }
 })
+
+const tiltOptions = {
+  speed: 50,
+  max: 10,
+  transition: true,
+  gyroscope: false,
+}
 </script>
 
 <template>
-  <div class="m-4 relative w-80 h-96 text-center flex shadow-xl flex-col justify-center items-center">
-    <img alt="skate-board" class="top-0 absolute z-10 rounded" src="@/assets/overlay.png"/>
-    <img :src="imgPath" alt="skate-board" class="top-0 rounded absolute"/>
-  </div>
+  <Tilt :options="tiltOptions" :parallax="true" class="m-4 relative w-[20rem] h-[24.85rem] text-center flex shadow-xl flex-col justify-center bg-none items-center rounded cursor-pointer">
+      <img alt="skate-board" class="shine top-0 absolute z-10" src="@/assets/overlay.png" style="transform: translateZ(20px)"/>
+      <img :src="imgPath" alt="skate-board" class="top-0 absolute" style="transform: translateZ(-20px)"/>
+  </Tilt>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 
+.shine {
+  position: relative;
+  overflow: hidden;
+  transition: all 0.3s;
+  &::before {
+    background: linear-gradient(
+                    to right,
+                    fade_out(#fff, 1) 0%,
+                    fade_out(#fff, 0.7) 100%
+    );
+    content: " ";
+    height: 120%;
+    transform: skewX(-25deg);
+    left: -85%;
+    position: absolute;
+    top: 0;
+    width: 50%;
+    z-index: 2;
+  }
+
+  &:hover {
+    box-shadow: 0 0 15px 15px rgba(0, 0, 0, .3), 0 0 0 2px white;
+    &::before {
+      animation: shine 0.5s;
+    }
+  }
+
+  @keyframes shine {
+    100% {
+      left: 125%;
+    }
+  }
+}
 </style>
---
+++ src/types/vanilla-tilt-vue.d.ts
@@ -0,0 +1,2 @@
+﻿declare module 'vanilla-tilt-vue';
+// https://www.npmjs.com/package/vanilla-tilt-vue
